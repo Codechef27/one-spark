7dd74de2d68553f2718c411b03f8685924cbda98
Lindsey Sherrill <lindsey350@aol.com>
2022-09-21 10:01:57 -0400

    personalBest

--- client/src/pages/Records/index.js
+++ client/src/pages/Records/index.js
@@ -1,7 +1,7 @@
-import {Tab, Tabs, Table } from 'react-bootstrap'
+import { Tab, Tabs, Table } from 'react-bootstrap'
 import { useQuery } from "@apollo/client";
 import { QUERY_USER, QUERY_USERS } from "../../utils/queries";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const Records = () => {
@@ -11,41 +11,78 @@ const Records = () => {
 
   const user = data?.user || {};
   const allUsers = data?.users || [];
+  const personalBest = []
 
+console.log(allUsers)
   //filter each game for lowest score 
 
+    const mcraft = user.records?.filter((item) => item.gameTitle === 'Minecraft')
+    const princesses = user.records?.filter((item) => item.gameTitle === 'Princesses')
 
-    // const mcraft= user.records?.filter((item) => item.gameTitle === 'Minecraft')
-    // const uniqueMcraft = mcraft.filter((c,index)=> {
-    //   return mcraft.indexOf(c) === index.gameTitle
-    // })
-    // uniqueMcraft.sort((a, b)=>{
-    //   return a.points - b.points
-    // })
-    
-    // console.log(uniqueMcraft)
+    const mpoints = []
+    const ppoints = []
+    const uniqueMcraft = mcraft?.filter(element => {
+      const duplicate = mpoints.includes(element.points);
+      if (!duplicate) {
+        mpoints.push(element.points)
+        return true
+      }
+      return false
+    })
+    const uniquePrincesses = princesses?.filter(element => {
+      const duplicate = ppoints.includes(element.points);
+      if (!duplicate) {
+        ppoints.push(element.points)
+        return true
+      }
+      return false
+    })
+
+    uniqueMcraft?.sort((a, b) => {
+      return a.points - b.points
+    })
+    uniquePrincesses?.sort((a, b) => {
+      return a.points - b.points
+    })
+
+    personalBest.push(uniqueMcraft[0], uniquePrincesses[0])
   
 
-//   const renderUserData = (data,_id) => {
-//     return (
-//       <tr key={data._id}>
-//         <td>{1}</td>
-//         <td>{data.records.gameTitle}</td>
-//         <td>{data.records.points}</td>
-//       </tr>
-//     );   
-//   }
 
-//   const renderAllUsersData = (data, _id) => {
-//     return (
-//       <tr key={_id}>
-//         <td>{1}</td>
-//         <td>{data.username}</td>
-//         <td>{data.records.gameTitle}</td>
-//         <td>{data.records.points}</td>
-//       </tr>
-//     );   
-//   }
+
+
+  //   // sort with asending order
+  //   return filter.sort((a, b) => {
+  //     if ( a.score === b.score){
+  //         return b.score - a.score;
+  //     } else{
+  //         return b.score - a.score;
+  //     }
+  // })
+
+  //console.log(uniqueMcraft)
+
+
+  //   const renderUserData = (data,_id) => {
+  //     return (
+  //       <tr key={data._id}>
+  //         <td>{1}</td>
+  //         <td>{data.records.gameTitle}</td>
+  //         <td>{data.records.points}</td>
+  //       </tr>
+  //     );   
+  //   }
+
+  //   const renderAllUsersData = (data, _id) => {
+  //     return (
+  //       <tr key={_id}>
+  //         <td>{1}</td>
+  //         <td>{data.username}</td>
+  //         <td>{data.records.gameTitle}</td>
+  //         <td>{data.records.points}</td>
+  //       </tr>
+  //     );   
+  //   }
 
 
 
@@ -54,7 +91,7 @@ const Records = () => {
       <Tab eventKey="yourRecords" title="Personal Best" borderless="true">
         <Table striped bordered hover size="sm" variant="dark" className="">
 
-           
+
           <thead>
             <tr>
               <th>#</th>
@@ -62,19 +99,19 @@ const Records = () => {
               <th>Best Moves</th>
             </tr>
           </thead>
-          <tbody> 
-          {user.records?.map(({id, gameTitle, points})=>(
-            <tr key={id}>
-                  <td>{id}</td>
-                    <td>{gameTitle}</td>
-                    <td>{points}</td>
-                  </tr>
-          ))}
+          <tbody>
+            {personalBest?.map(({ id, gameTitle, points }) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{gameTitle}</td>
+                <td>{points}</td>
+              </tr>
+            ))}
           </tbody>
-   
-          
+
+
         </Table>
-      
+
       </Tab>
       <Tab eventKey="leaderboard" title="Leaderboard">
         <Table striped bordered hover size="sm" variant="dark">
@@ -88,7 +125,12 @@ const Records = () => {
           </thead>
           <tbody>
             <tr>
-            {/* {{allUsers.map(renderAllUsersData)} */}
+
+              
+                <td>coming</td>
+                <td>soon</td>
+                <td>!!!</td>
+              
             </tr>
           </tbody>
         </Table>
