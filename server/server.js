64a18ec55e38d668437d1323387d1c4edc123109
Lindsey Sherrill <lindsey350@aol.com>
2022-09-21 10:19:21 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -18,21 +17,17 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
-
-
-
 
   db.once("open", () => {
     app.listen(PORT, () => {
@@ -43,4 +38,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
     });
   });
 };
+
 startApolloServer(typeDefs, resolvers);
