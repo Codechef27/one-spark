94636fad26634996668afe802ee8d39183a69320
BradleyK <bradleyjkennett@gmail.com>
2023-02-09 12:44:58 -0500

    aws deploy attempt

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -4,21 +4,11 @@ const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
 // Set the region
-AWS.config.update({ region: 'us-east-2' });
+AWS.config.update({ region: 'us-east-1' });
 
-// Create S3 service object
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-// Create the parameters for calling createBucket
-var bucketParams = {
-    Bucket: 'user-scores-' + uuidv4(),
-  };
 
-// call S3 to create the bucket
-s3.createBucket(bucketParams, (err, data) => {
-    if (err) {
-      console.log('Error', err);
-    } else {
-      console.log('Success');
-    }
-  });  
+
+
+
+ 
